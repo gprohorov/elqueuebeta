f07b3c2d75ac5ce6bc1bec5e74db0c559e5d6fb1
BVH77 <bvh.box@gmail.com>
2018-04-01 11:32:28 +0300

    Added persons template

--- front/src/app/persons/persons.component.ts
+++ front/src/app/persons/persons.component.ts
@@ -1,6 +1,6 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Person } from '../_models/index';
-import { PersonService } from '../_services/index';
+import { PersonService, AlertService } from '../_services/index';
 
 @Component({
   moduleId: module.id,
@@ -11,17 +11,33 @@ export class PersonsComponent implements OnInit {
 
   persons: Person[] = [];
 
-  constructor(private personService: PersonService) {
+  constructor(private personService: PersonService, private alertService: AlertService) {
   }
 
   ngOnInit() {
       this.loadAllPersons();
+  }
+  
+  doSearch() {
+    this.alertService.error('На стадії розробки');
+  }
+
+  createPerson() {
+    this.alertService.error('На стадії розробки');
+  }
+  
+  editPerson(id: number) {
+    this.alertService.error('На стадії розробки');
   }
 
   deletePerson(id: number, name: string) {
       if (confirm('Видалити персону "' + name + '" ?')) this.personService.deletePerson(id).subscribe(() => { this.loadAllPersons() });
   }
 
+  getFullName(person: Person) {
+    return [person.firstName, person.patronymic, person.lastName].join(' ');
+  }
+  
   private loadAllPersons() {
       this.personService.getAll().subscribe(persons => { this.persons = persons; });
   }
