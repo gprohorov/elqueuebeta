f8053981da21dd7a0bf418644ecfecc05282da6f
BVH77 <bvh.box@gmail.com>
2018-04-03 20:56:51 +0300

    Config modified

--- front/src/default.config.ts
+++ front/src/default.config.ts
@@ -1,3 +1,5 @@
 export const config = {
-  api_path: 'http://localhost:8088/api'
+  // api_path: 'http://mednean.e-head.ru:8088/api'
+  // api_path: 'http://192.168.1.2:8088/api'
+  api_path: 'http://' + window.location.hostname + ':8088/api'
 }
