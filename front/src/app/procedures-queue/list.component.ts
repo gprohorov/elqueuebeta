0f4aa472cf5694817f42c746dfeed3cfe0114444
BVH77 <bvh.box@gmail.com>
2018-04-11 18:32:36 +0300

    F: Cosmetic changes queue page

--- front/src/app/procedures-queue/list.component.ts
+++ front/src/app/procedures-queue/list.component.ts
@@ -16,14 +16,12 @@ export class ProceduresQueueListComponent implements OnInit {
   sub: Subscription;
   items: any[] = [];
   loading = false;
-  rows = [];
   Statuses = Statuses;
   StatusesArr = StatusesArr;
   Activity = Activity;
   ActivityArr = ActivityArr;
 
   public myOptions: NgxMasonryOptions = {
-    //itemSelector: '.masonry-item',
     transitionDuration: '0.2s',
     columnWidth: 250,
     fitWidth: true,
@@ -42,15 +40,8 @@ export class ProceduresQueueListComponent implements OnInit {
   
   ngOnDestroy() {
     this.sub.unsubscribe();
-    // this.subQueue.forEach(function(item) {
-      // item.unsubscribe();
-    // });
   } 
-  
-  delete(id: number, name: string) {
-    // if (confirm('Видалити "' + name + '" ?')) this.service.delete(id).subscribe(() => { this.load() });
-  }
-  
+
   getFullName(item: Person) {
     return [item.lastName, item.firstName, item.patronymic].join(' ');
   }
@@ -70,12 +61,4 @@ export class ProceduresQueueListComponent implements OnInit {
     this.sub = this.service.getTails().subscribe(data => { this.items = data; this.loading = false; });
   }
   
-  /*
-  loadAll() {
-    this.items.forEach(function(item) {
-      this.loading = true;
-      this.subQueue.push(this.servicePatients.getAllByProcedure(item.id).subscribe(data => { item.queue = data; this.loading = false; }));
-    }, this);
-  }
-  */
 }
