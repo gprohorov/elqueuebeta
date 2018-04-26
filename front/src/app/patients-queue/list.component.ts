348d7edfea89ed04ff31881bfd9620bdb67ca216
BVH77 <bvh.box@gmail.com>
2018-04-26 13:07:45 -0700

    F: Sorting patients queue by startActivity.

--- front/src/app/patients-queue/list.component.ts
+++ front/src/app/patients-queue/list.component.ts
@@ -66,7 +66,17 @@ export class PatientsQueueListComponent implements OnInit {
 
   load(search: string = '') {
     this.loading = true;
-    this.sub = this.service.getAll().subscribe(data => { this.items = data; this.loading = false; });
+    this.sub = this.service.getAll().subscribe(data => {
+      this.items = data.sort(function(a, b) {
+        console.log(a.startActivity, b.startActivity);
+        // return a.startActivity < b.startActivity;
+        const x = a.startActivity, y = b.startActivity;
+        if (x < y) { return -1; }
+        if (x > y) { return 1; }
+        return 0;
+      });
+      this.loading = false;
+    });
   }
 
 }
