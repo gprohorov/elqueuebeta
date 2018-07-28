f80b9b004e8f047599af953f8437bcd1f999b593
BVH77 <bvh.box@gmail.com>
2018-07-28 10:16:47 +0300

    F: Hotel table unfinished

--- front/src/app/_services/hotel.service.ts
+++ front/src/app/_services/hotel.service.ts
@@ -9,28 +9,33 @@ import { Procedure } from '../_models/index';
 
 @Injectable()
 export class HotelService {
-  private bookingUrl        = config.api_path + '/workplace/hotel/booking/';
-  private koikaListUrl      = config.api_path + '/workplace/hotel/koika/list/';
-  private createRecordUrl   = config.api_path + '/workplace/hotel/record/create/';
+    private bookingUrl = config.api_path + '/workplace/hotel/booking/';
+    private koikaMapUrl = config.api_path + '/workplace/hotel/koika/map/';
+    private koikaListUrl = config.api_path + '/workplace/hotel/koika/list/';
+    private createRecordUrl = config.api_path + '/workplace/hotel/record/create/';
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  getBooking() {
-      return this.http.get(this.bookingUrl).pipe(catchError(this.handleError));
-  }
+    getBooking() {
+        return this.http.get(this.bookingUrl).pipe(catchError(this.handleError));
+    }
 
-  getKoikaList() {
-      return this.http.get(this.koikaListUrl).pipe(catchError(this.handleError));
-  }
-  
-  createRecord(record: any) {
-      return this.http.post(this.createRecordUrl, record).pipe(catchError(this.handleError));
-  }
-  
-  // Implement a method to handle errors if any
-  private handleError(err: HttpErrorResponse | any) {
-    console.error('An error occurred', err);
-    return Observable.throw(err.message || err);
-  }
+    getKoikaMap() {
+        return this.http.get(this.koikaMapUrl).pipe(catchError(this.handleError));
+    }
+
+    getKoikaList() {
+        return this.http.get(this.koikaListUrl).pipe(catchError(this.handleError));
+    }
+
+    createRecord(record: any) {
+        return this.http.post(this.createRecordUrl, record).pipe(catchError(this.handleError));
+    }
+
+    // Implement a method to handle errors if any
+    private handleError(err: HttpErrorResponse | any) {
+        console.error('An error occurred', err);
+        return Observable.throw(err.message || err);
+    }
 
 }
