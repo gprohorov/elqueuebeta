cc0b81345b25c8b7ffe381741c23b43666d519dc
BVH77 <bvh.box@gmail.com>
2018-08-10 23:03:04 +0300

    F: added button Next date in patients queue list fix

---
+++ front/src/app/patient/assign-procedures-on-date.modal.component.ts
@@ -0,0 +1,49 @@
+import { Component, ComponentRef, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { IModalDialog, IModalDialogButton, IModalDialogOptions } from 'ngx-modal-dialog';
+
+import { PatientService, AlertService } from '../_services/index';
+
+@Component({
+    templateUrl: './assign-procedures-on-date.modal.component.html',
+})
+export class PatientAssignProceduresOnDateModalComponent implements IModalDialog {
+
+    data: any;
+    sub: Subscription;
+
+    @ViewChild('f') myForm;
+    constructor(
+        private alertService: AlertService,
+        private patientService: PatientService
+    ) { }
+
+    dialogInit(reference: ComponentRef<IModalDialog>, options: Partial<IModalDialogOptions<any>>) {
+        options.actionButtons = [{
+            text: 'Призначити',
+            onAction: () => {
+                return this.submit(this.myForm, options);
+            }
+        }, { text: 'Скасувати', buttonClass: 'btn btn-secondary' }];
+        this.data = options.data;
+        this.data.date = (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000 + 24*60*60*1000)).toISOString().slice(0, -14);
+        this.data.time = 9;
+    }
+
+    submit(f, options) {
+        f.submitted = true;
+        if (!f.form.valid) return false;
+
+        this.patientService.assignProceduresOnDate(
+            this.data.patientId, this.data.date
+        ).subscribe(() => {
+            this.alertService.success('Пацієнта ' + this.data.patientName
+                + ' назначено на процедури на наступну дату.');
+            options.closeDialogSubject.next();
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.sub) this.sub.unsubscribe();
+    }
+}
