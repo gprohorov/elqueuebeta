b9a793be4ba4971d81bdf0f93f0f24ba4e3e0ac6
BVH77 <bvh.box@gmail.com>
2018-08-31 14:10:30 +0300

    B: Patients list implemented native query with limit & sorting using Ukrainian locale.

--- src/main/java/com/med/repository/patient/PatientRepository.java
+++ src/main/java/com/med/repository/patient/PatientRepository.java
@@ -1,7 +1,9 @@
 package com.med.repository.patient;
 
 import com.med.model.Patient;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +13,10 @@ import java.util.List;
  */
 @Repository
 public interface PatientRepository extends MongoRepository<Patient, String>{
-  //  List<Patient> findByLastName(String lastName);
-    List<Patient> findByBalanceLessThan(long balance);
+
+	List<Patient> findByBalanceLessThan(long balance);
+    
+    @Query(value="{ 'person.fullName': { $regex: ?0, $options: 'i' } }")
+    List<Patient> findByThePersonFullName(String fullName);
 
 }
