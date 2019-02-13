cdaa88cd4d4e25e9eea3be059c4692a98b7f8ebe
george <yuramail222@gmail.com>
2019-02-13 13:44:58 +0200

    showSummaryForPeriodForDoctor

--- src/main/java/com/med/repository/SalaryDailyRepository.java
+++ src/main/java/com/med/repository/SalaryDailyRepository.java
@@ -4,5 +4,10 @@ import com.med.model.SalaryDaily;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface SalaryDailyRepository extends MongoRepository<SalaryDaily, String> {}
+public interface SalaryDailyRepository extends MongoRepository<SalaryDaily, String> {
+    List<SalaryDaily> findByDateBetweenAndDoctorId(LocalDate from, LocalDate to, int doctorId);
+}
