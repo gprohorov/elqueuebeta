9d195b913279f6249095f7de313211c3c44fbfaa
george <yuramail222@gmail.com>
2019-03-01 10:02:35 +0200

    initWorkDay(

--- src/main/java/com/med/services/WorkDayService.java
+++ src/main/java/com/med/services/WorkDayService.java
@@ -1,8 +1,12 @@
 package com.med.services;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import com.med.model.Activity;
+import com.med.model.Talon;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.med.model.workday.WorkDay;
@@ -13,6 +17,15 @@ public class WorkDayService  {
 	
     @Autowired
     WorkDayRepository repository;
+
+    @Autowired
+    CashBoxService cashBoxService;
+
+    @Autowired
+    TalonService talonService;
+
+    @Autowired
+    PatientService patientService;
 
     public WorkDay create(WorkDay workDay) {
         return repository.save(workDay);
@@ -32,5 +45,23 @@ public class WorkDayService  {
     
     public WorkDay update(WorkDay workDay) {
         return repository.save(workDay);
+    }
+    @Scheduled(cron = "0 0 7 * * *")
+    public void initWorkDay(){
+        WorkDay workDay = new WorkDay(LocalDate.now());
+        workDay.setSumAtStart(cashBoxService.getCashBox());
+        int assigned = (int) talonService.getTalonsForToday().stream()
+                .map(Talon::getPatientId).distinct().count();
+        workDay.setAssignedPatients(assigned);
+        this.create(workDay);
+    }
+    @Scheduled(cron = "0 0 10 * * *")
+    public void setWorkDayStart(){
+        WorkDay workDay = this.getWorkDay(LocalDate.now());
+        LocalDateTime start = talonService.getTalonsForToday().stream()
+                .filter(talon -> talon.getActivity().equals(Activity.EXECUTED))
+                .map(Talon::getStart).findFirst().orElse(null);
+        workDay.setStart(start);
+        this.update(workDay);
     }
 }
