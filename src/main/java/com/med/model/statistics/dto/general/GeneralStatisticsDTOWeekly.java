e65d0cd7ffbdf4a2ca1094f0e4aef08447c8857e
george <yuramail222@gmail.com>
2019-09-05 20:58:45 +0300

    GeneralStatisticsDTOWeekly

---
+++ src/main/java/com/med/model/statistics/dto/general/GeneralStatisticsDTOWeekly.java
@@ -0,0 +1,17 @@
+package com.med.model.statistics.dto.general;
+
+import java.time.LocalDate;
+
+public class GeneralStatisticsDTOWeekly {
+
+    private String week;
+    private int patients;
+    private int doctors;
+    private long cash;
+    private long card;
+    private long bill;
+    private long discount;
+    private long debt;
+
+
+}
