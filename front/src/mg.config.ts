23c7ee156b2c42d509ecf7b882dbe29b4a9169ba
BVH77 <bvh.box@gmail.com>
2020-01-04 01:08:47 +0200

    Update mg.config.ts

--- front/src/mg.config.ts
+++ front/src/mg.config.ts
@@ -1,6 +1,6 @@
 const api_protocol  = 'http',
       api_host      = window.location.hostname,
-      api_port      = '8077',
+      api_port      = '8088',
       api_suffix    = '/api';
 export const config = {
   api_path: api_protocol + '://' + api_host + ':' + api_port + api_suffix,
