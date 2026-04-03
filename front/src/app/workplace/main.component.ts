fec57c7a581d3392f87b487fd6a63672da64ae15
gprohorov <g.prokhorov@indevlab.com>
2026-04-03 21:59:55 +0300

    icons

--- front/src/app/workplace/main.component.ts
+++ front/src/app/workplace/main.component.ts
@@ -23,7 +23,7 @@ export class WorkplaceMainComponent implements OnInit, OnDestroy {
 
     public myOptions: NgxMasonryOptions = {
         transitionDuration: '0.2s',
-        columnWidth: 400,
+        columnWidth: 350,
         fitWidth: true,
         horizontalOrder: true,
         gutter: 20
